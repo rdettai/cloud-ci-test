bd4194abe246359ae7c9c8b76f09fe3aa9c4d107
rdettai <rdettai@gmail.com>
2022-03-15 17:52:22 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">=1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
+  }
+}
+
+terraform {
+  backend "local" {
+    path = ".terraform/state/terraform.tfstate"
+  }
+}
+
+
+provider "aws" {
+  region = "eu-west-1
+  default_tags {
+    tags = {
+      module      = "ci-test
+      provisioner = "terraform"
+    }
+  }
+}
+
+resource "aws_ecs_cluster" "fargate_cluster" {
+  name               = "test-ci-dummy-resource"
+}
